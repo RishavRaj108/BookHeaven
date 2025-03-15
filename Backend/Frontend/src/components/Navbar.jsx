61146323e115e03fabf8358803bb227460a9d342
RishavRaj108 <rishavraj4411@gmail.com>
2025-03-15 17:41:07 +0530

    New Changes

--- Backend/Frontend/src/components/Navbar.jsx
+++ Backend/Frontend/src/components/Navbar.jsx
@@ -54,7 +54,13 @@ function Navbar() {
   );
   return (
     <>
-    
+      <div
+        className={` max-w-screen-2xl w-screen overflow-hidden container mx-auto md:px-20 px-3 dark:bg-slate-800 dark:text-white fixed top-0 left-0 right-0 z-50 ${
+          sticky
+            ? "sticky-navbar shadow-md bg-base-200 dark:bg-slate-700 dark:text-white duration-300 transition-all ease-in-out"
+            : ""
+        }`}
+      >
         <div className="navbar ">
           <div className="navbar-start">
             <div className="dropdown">
